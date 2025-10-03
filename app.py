c69fe8212e108ba66f2ba22fab395a7c9a729609
Jeet Bhardwaj <jeet90008@gmail.com>
2025-10-03 12:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def get_model():
                 print("Attempting to download model...")
                 try:
                     model_path = hf_hub_download(
-                        repo_id='YOUR_USERNAME/age_gender_final_model',  # Replace with your repo ID
+                        repo_id='JeetBhardwaj07/age-gender-predictor',  # Replace with your repo ID
                         filename='age_gender_final_model.h5',
                         local_dir='/tmp',
                         local_dir_use_symlinks=False
